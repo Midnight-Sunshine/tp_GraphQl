bffdc7be610c532f43385f1cbf8e9cbdf15d3037
midnight <midnight@email.com>
2024-12-12 09:23:43 +0100

    initial commit

---
+++ src/main/java/com/graphql/TPGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPGraphQlApplication.class, args);
+	}
+
+}
